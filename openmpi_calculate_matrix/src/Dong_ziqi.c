dc5bbe2824e0a54a48033656ef7ebb92968c3938
ziqidong <dongziqi1230@163.com>
2019-12-31 12:25:08 +0800

    modify comment

--- openmpi_calculate_matrix/src/Dong_ziqi.c
+++ openmpi_calculate_matrix/src/Dong_ziqi.c
@@ -101,7 +101,7 @@ int main(int argc, char *argv[]){
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
     int world_size;
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-  //---------------------------------------------------------delcare matrix and vector
+  //---------------------------------------------------------declare matrix and vector
     int * vector;
     int * matrix;
     int * result;
